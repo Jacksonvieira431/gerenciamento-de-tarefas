5472c8a25f516fa5124f29238ad70b92abc2c250
Jacksonvieira431 <jacksonhudson431@gmail.com>
2023-10-08 19:45:15 -0300

    checkbox

--- todo.py
+++ todo.py
@@ -11,14 +11,31 @@ class ToDo:
         self.page.title = "ToDo App"
         self.main_page()
 
+
+    def tasks_container(self):
+        return ft.Container(
+            height=self.page.height * 0.8,
+            content=ft.Column(controls=[ft.Checkbox(label="Tarefa 1", value=True)]))
+
     def main_page(self):
-        input_task = ft.TextField(hint_text="Digite aqui uma tarefa")
+        input_task = ft.TextField(hint_text="Digite aqui uma tarefa", expand=True)
         input_bar = ft.Row(
             controls=[
             input_task,
             ft.FloatingActionButton(icon=ft.icons.ADD)
             ]
         )
-        self.page.add(input_bar)
+
+        tabs = ft.Tabs(
+            selected_index=0,
+            tabs=[
+                ft.Tab(text="todos"),
+                ft.Tab(text="Em andamento"),
+                ft.Tab(text="Concluídos")
+            ]
+        )
+
+        tasks = self.tasks_container()
+        self.page.add(input_bar, tabs, tasks)
 
 ft.app(target=ToDo)
